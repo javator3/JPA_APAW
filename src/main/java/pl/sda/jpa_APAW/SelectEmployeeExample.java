bc68d2185bb82e2437e232836eb3276c3e10652f
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-03-31 12:54:23 +0200

    jpql w akcji all examples done

--- src/main/java/pl/sda/jpa_APAW/SelectEmployeeExample.java
+++ src/main/java/pl/sda/jpa_APAW/SelectEmployeeExample.java
@@ -10,14 +10,46 @@ public class SelectEmployeeExample {
 
         EntityManager entityManager = Engin.getEntityManager();
         
-        entityManager.getTransaction().begin();
-        
-        TypedQuery<Employee> query = entityManager.createQuery("select e from Employee e", Employee.class);
+//        entityManager.getTransaction().begin();
+//
+//        TypedQuery<Employee> query = entityManager.createQuery("select e from Employee e", Employee.class);
+//
+//        List<Employee> resultList = query.getResultList();
+//resultList.stream()
+//        .forEach(employee -> System.out.println("employee = " + employee));
+//
+//        entityManager.getTransaction().commit();
 
-        List<Employee> resultList = query.getResultList();
-resultList.stream()
-        .forEach(employee -> System.out.println("employee = " + employee));
-        
+//        entityManager.getTransaction().begin();
+//
+//        TypedQuery<Employee> query = entityManager.createQuery("select e from Employee e order by name asc", Employee.class);
+//
+//        List<Employee> resultList = query.getResultList();
+//        resultList.stream()
+//                .forEach(employee -> System.out.println("employee = " + employee));
+//
+//        entityManager.getTransaction().commit();
+
+
+//        entityManager.getTransaction().begin();
+//
+//        TypedQuery<Employee> query = entityManager.createQuery("select e from Employee e where e.salary > 2000", Employee.class);
+//
+//        List<Employee> resultList = query.getResultList();
+//        resultList.stream()
+//                .forEach(employee -> System.out.println("employee = " + employee));
+//
+//        entityManager.getTransaction().commit();
+
+        entityManager.getTransaction().begin();
+
+        TypedQuery<String> query = entityManager.createQuery("select e.name from Employee e where e.name like 'A%'", String.class);
+
+        List<String> resultList = query.getResultList();
+        resultList.stream()
+                .forEach(employee -> System.out.println("employee = " + employee));
+
         entityManager.getTransaction().commit();
+
     }
 }
